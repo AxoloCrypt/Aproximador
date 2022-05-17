be4c843ce8cb17e58ef839dbe9c40f18f947a7ac
AxoloCrypt <axolocrypt@outlook.com>
2022-05-17 17:09:31 -0600

    Update

--- src/main/java/com/aproximador/controllers/Controller.java
+++ src/main/java/com/aproximador/controllers/Controller.java
@@ -1,15 +1,14 @@
 package com.aproximador.controllers;
 
-import com.aproximador.data.*;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import com.aproximador.data.Connector;
+import com.aproximador.data.Materials;
+import com.aproximador.data.Services;
+import javafx.scene.layout.VBox;
 
 public class Controller
 {
     private final Materials materials = new Materials();
     private final Services services = new Services();
     private final Connector connector = new Connector("juca", "g*$0Pe$h18cyiyJC");
-
 
 }
