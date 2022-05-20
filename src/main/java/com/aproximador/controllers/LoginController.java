b8bd47a90f4b613b3581b1e920250b8de6f76e9d
Luis Gomes <luis.arturo.gomess@gmail.com>
2022-05-19 22:17:22 -0600

    button correction methods

--- src/main/java/com/aproximador/controllers/LoginController.java
+++ src/main/java/com/aproximador/controllers/LoginController.java
@@ -2,18 +2,25 @@ package com.aproximador.controllers;
 
 import com.aproximador.app.App;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.Pane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class LoginController {
+
+
+    @FXML
     private Button btnLogin;
+
+    @FXML
+    public Button btnExit;
+
 
     public void openApp(ActionEvent actionEvent) throws IOException {
         Parent root = FXMLLoader.load(App.class.getResource("app.fxml"));
@@ -22,9 +29,14 @@ public class LoginController {
         Stage primaryStage = new Stage();
         primaryStage.setTitle("Aproximador");
         primaryStage.setScene(LoginScene);
-        primaryStage.initModality(Modality.WINDOW_MODAL);
-        //primaryStage.initOwner(btnLogin.getScene().getWindow());
+        primaryStage.initModality(Modality.APPLICATION_MODAL);
+        primaryStage.initOwner(btnLogin.getScene().getWindow());
         primaryStage.show();
+        Stage primaryStage = (Stage) this.btnLogin.getScene().getWindow();
+        primaryStage.close();;
     }
 
+    public void ExitApp(ActionEvent actionEvent) {
+        System.exit(0);
+    }
 }
