04c8d63b063ebc015e6fcd2be4c1eb3419a1fb54
AxoloCrypt <axolocrypt@outlook.com>
2022-05-20 12:57:56 -0600

    Add record popup implemented

--- src/main/java/com/aproximador/controllers/AddRecordController.java
+++ src/main/java/com/aproximador/controllers/AddRecordController.java
@@ -5,6 +5,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Border;
@@ -17,24 +19,42 @@ import java.util.ResourceBundle;
 public class AddRecordController implements Initializable
 {
     @FXML
-    private Label lblRecord;
-    @FXML
-    private Label lblCost;
-    @FXML
-    private Button btnOk;
+    public Label lblRecord;
+    public Label lblCost;
+    public Button btnOk;
+    public TextField txtName;
+    public TextField txtCost;
+    public TextArea txtDescription;
 
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        btnOk.setOnKeyPressed(new EventHandler<KeyEvent>() {
-            @Override
-            public void handle(KeyEvent event) {
-                if(event.getCode() == KeyCode.ENTER){
-                    Stage stage = (Stage) btnOk.getScene().getWindow();
-                    stage.close();
-                }
+
+        btnOk.setOnKeyPressed(event -> {
+            if(event.getCode() == KeyCode.ENTER){
+                Stage stage = (Stage) btnOk.getScene().getWindow();
+                stage.close();
             }
         });
+
+        txtName.setOnKeyPressed(event -> {
+            if(event.getCode() == KeyCode.ENTER)
+                txtCost.requestFocus();
+        });
+
+        txtCost.setOnKeyPressed(event -> {
+            if(event.getCode() == KeyCode.ENTER)
+                txtDescription.requestFocus();
+        });
+
+        txtDescription.setOnKeyPressed(event -> {
+            if (event.getCode() == KeyCode.ENTER){
+                txtDescription.setDisable(true);
+                Stage stage = (Stage) txtDescription.getScene().getWindow();
+                stage.close();
+            }
+        });
+
     }
 }
