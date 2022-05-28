d369bc0e65a3c7d377c3cd477fe40295d0231fd1
AxoloCrypt <axolocrypt@outlook.com>
2022-05-28 15:39:34 -0600

    Increment and decrement the quantity of a used record in an approximation implemented

--- src/main/java/com/aproximador/view/RecordPane.java
+++ src/main/java/com/aproximador/view/RecordPane.java
@@ -9,7 +9,6 @@ import javafx.scene.control.*;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 
 import java.math.BigDecimal;
 
@@ -23,6 +22,9 @@ public class RecordPane extends DialogPane {
     private Button btnRemove;
     private Button btnAdd;
     private TextField txtQuantity;
+    private String description;
+    private BigDecimal originalCost;
+    private BigDecimal currentCost;
 
     public RecordPane(String recordName, String cost, String description, Controller controller, boolean isMaterial){
 
@@ -47,12 +49,11 @@ public class RecordPane extends DialogPane {
             Tab currentAproximationTab = controller.getTabAproximations().getSelectionModel().getSelectedItem();
             int tabIndex = controller.getTabAproximations().getSelectionModel().getSelectedIndex();
 
-
             Node node = currentAproximationTab.getContent();
 
             VBox vBoxRecords = (VBox) node.lookup("VBox");
 
-            vBoxRecords.getChildren().add(new RecordPane(lblName.getText()));
+            vBoxRecords.getChildren().add(new RecordPane(lblName.getText(), new BigDecimal(lblCost.getText().replace("$", "")), lblDescription.getText(), controller, isMaterial, true));
 
             if (isMaterial)
                 controller.getAproximations().get(tabIndex).getRecords().add(new Materials(lblName.getText(),
@@ -72,17 +73,94 @@ public class RecordPane extends DialogPane {
 
     }
 
-    public RecordPane(String recordName){
+    public RecordPane(String recordName, BigDecimal cost, String description, Controller controller, boolean isMaterial, boolean isUsed){
         this.setHeader(new Label(recordName));
+        originalCost = cost;
+        currentCost = originalCost;
+        this.description = description;
 
         btnRemove = new Button("R");
+        btnRemove.setOnAction(event -> {
+            int tabIndex = controller.getTabAproximations().getSelectionModel().getSelectedIndex();
+            int currentAmount = Integer.parseInt(txtQuantity.getText());
+
+            currentAmount = currentAmount > 1 ? currentAmount - 1 : 1;
+
+            if(currentAmount != 1){
+
+                if(isMaterial){
+                    int selectedMaterial = controller.getAproximations().get(tabIndex).getRecords().indexOf(new Materials(recordName,
+                            currentCost, this.description));
+
+                    currentCost = currentCost.subtract(originalCost);
+
+                    controller.getAproximations().get(tabIndex).getRecords().get(selectedMaterial).setUnitCost(currentCost);
+                }
+                else{
+                    int selectedService = controller.getAproximations().get(tabIndex).getRecords().indexOf(new Services(recordName,
+                            currentCost, this.description));
+
+                    currentCost = currentCost.subtract(originalCost);
+
+                    controller.getAproximations().get(tabIndex).getRecords().get(selectedService).setUnitCost(currentCost);
+                }
+            }
+            else{
+
+                btnRemove.setDisable(true);
+
+                if(isMaterial){
+                    int selectedMaterial = controller.getAproximations().get(tabIndex).getRecords().indexOf(new Materials(recordName,
+                            currentCost, this.description));
+
+                    currentCost = currentCost.subtract(originalCost);
+
+                    controller.getAproximations().get(tabIndex).getRecords().get(selectedMaterial).setUnitCost(currentCost);
+                }
+                else{
+                    int selectedService = controller.getAproximations().get(tabIndex).getRecords().indexOf(new Services(recordName,
+                            currentCost, this.description));
+
+                    currentCost = currentCost.subtract(originalCost);
+
+                    controller.getAproximations().get(tabIndex).getRecords().get(selectedService).setUnitCost(currentCost);
+                }
+            }
+
+            txtQuantity.setText(String.valueOf(currentAmount));
+        });
+        btnRemove.setDisable(true);
 
         txtQuantity = new TextField("1");
-        txtQuantity.setMaxSize(20, 10);
-        txtQuantity.setEditable(false);
+        txtQuantity.setMaxSize(50, 10);
         txtQuantity.setFocusTraversable(false);
 
         btnAdd = new Button("Add");
+        btnAdd.setOnAction(event -> {
+            int tabIndex = controller.getTabAproximations().getSelectionModel().getSelectedIndex();
+            int currentAmount = Integer.parseInt(txtQuantity.getText());
+
+            currentAmount += 1;
+            btnRemove.setDisable(false);
+
+            if(isMaterial){
+                int selectedMaterial = controller.getAproximations().get(tabIndex).getRecords().indexOf(new Materials(recordName,
+                        currentCost, this.description));
+
+                currentCost = currentCost.add(originalCost);
+
+                controller.getAproximations().get(tabIndex).getRecords().get(selectedMaterial).setUnitCost(currentCost);
+            }
+            else{
+                int selectedService = controller.getAproximations().get(tabIndex).getRecords().indexOf(new Services(recordName,
+                        currentCost, this.description));
+
+                currentCost = currentCost.add(originalCost);
+
+                controller.getAproximations().get(tabIndex).getRecords().get(selectedService).setUnitCost(currentCost);
+            }
+            txtQuantity.setText(String.valueOf(currentAmount));
+        });
 
         FlowPane flowPane = new FlowPane();
         flowPane.setAlignment(Pos.CENTER_RIGHT);
