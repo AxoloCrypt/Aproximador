e1607aa484834232afc19342c9f32f7a37827f68
AxoloCrypt <axolocrypt@outlook.com>
2022-05-28 22:56:14 -0600

    View history approximations in the center tab implemented (need for testing)

--- src/main/java/com/aproximador/view/AproximationPane.java
+++ src/main/java/com/aproximador/view/AproximationPane.java
@@ -2,22 +2,28 @@ package com.aproximador.view;
 
 
 import com.aproximador.controllers.Controller;
+import com.aproximador.data.Aproximation;
+import com.aproximador.data.History;
 import javafx.geometry.Pos;
 import javafx.scene.control.DialogPane;
 import javafx.scene.control.Label;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 public class AproximationPane extends DialogPane {
-    private Label lblName;
-    private Label lblDate;
 
-    public AproximationPane(String name, LocalDate dateCreation, Controller controller) {
+    private final LocalDateTime dateCreation;
 
-        lblName = new Label(name);
+    public AproximationPane(String name, LocalDateTime dateCreation, Controller controller) {
 
-        lblDate = new Label(dateCreation.toString());
+        this.dateCreation = dateCreation;
+
+        Label lblName = new Label(name);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+        Label lblDate = new Label(sdf.format(dateCreation));
         lblDate.setAlignment(Pos.CENTER_RIGHT);
 
         this.setHeader(lblName);
@@ -25,8 +31,19 @@ public class AproximationPane extends DialogPane {
 
         this.setOnMouseClicked(event -> {
 
-        });
+            for (Aproximation aproximation : controller.getHistory().getSavedAproximations()){
 
+                if (aproximation.getDateCreation().isEqual(this.dateCreation)){
+
+                    AproximationTab aproximationTab = new AproximationTab(aproximation.getName(),
+                            aproximation.getNumberMaterials(), aproximation.getNumberServices(),aproximation.getTotalCost().toString(), aproximation.getRecords());
+
+                    controller.getTabAproximations().getTabs().add(aproximationTab);
+                }
+
+            }
+
+        });
     }
 
 }
