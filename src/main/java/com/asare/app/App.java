a15533a110da96b6742d7b8dc488d5bc09a207ad
AxoloCrypt <axolocrypt@outlook.com>
2022-06-11 18:49:28 -0600

    Update

--- src/main/java/com/asare/app/App.java
+++ src/main/java/com/asare/app/App.java
@@ -18,7 +18,7 @@ public class App extends Application
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("login.fxml"));
 
 
-        primaryStage.getIcons().add(new Image("https://github.com/AxoloCrypt/Asare/blob/test/src/main/resources/com/asare/images/Logo.png?raw=true"));
+        primaryStage.getIcons().add(new Image("https://github.com/AxoloCrypt/Asare/blob/main/src/main/resources/com/asare/images/Logo.png?raw=true"));
 
         Scene scene = new Scene(fxmlLoader.load());
         primaryStage.setTitle("Asare");
