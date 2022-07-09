07e16a460ff9a13940a80fbb779d2c7c87d99bca
JuanCarlosCardona <juan.cardona@train.ia.center>
2022-07-09 17:22:11 -0600

    Test dist of app

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,10 +4,7 @@ module com.asare {
     requires javafx.web;
 
     requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires validatorfx;
     requires org.kordamp.ikonli.javafx;
-    requires eu.hansolo.tilesfx;
     requires java.sql;
 
     opens com.asare.app to javafx.fxml;
